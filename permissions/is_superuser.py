3d64850bd88f192aebb6ab43bba36b358a824c4a
Mohamed naser <75080962+mn9142001@users.noreply.github.com>
2023-11-30 10:50:15 +0200

    Update is_superuser.py

--- permissions/is_superuser.py
+++ permissions/is_superuser.py
@@ -3,5 +3,5 @@ from rest_framework.permissions import IsAuthenticated
 class IsSuperuser(IsAuthenticated):
 
     def has_permission(self, request, view):
-        is_logged = super().has_permission(request, view) 
-        return is_logged and request.user.is_superuser
+        is_authenticated = super().has_permission(request, view) 
+        return is_authenticated and request.user.is_superuser
