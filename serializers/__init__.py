6f6e457d45277dc81db05de912157033694004d9
Mohamed naser <75080962+mn9142001@users.noreply.github.com>
2023-11-30 10:49:32 +0200

    Update __init__.py

--- serializers/__init__.py
+++ serializers/__init__.py
@@ -1,3 +1,2 @@
 from .auth import SignUpSerializer, JWTLoginSerializer, PasswordSendResetSerializer, PasswordChangeSerializer, PasswordResetChangeSerializer, PasswordResetVerifySerializer
 from .user import UserLoggedInSerializer
-from .utils import PromoteUserPermissionSerializer
